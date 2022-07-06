803e6982320fef213e0e0291ba55f00b982659f6
Dominykas <dsmicius@gmail.com>
2022-07-06 15:30:39 +0300

    created HeaderContainer and FooterContainer

---
+++ blog-fron-end/src/containers/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FooterContainer from "./FooterContainer/FooterContainer";
+import HeaderContainer from "./Header/HeaderContainer";
+
+function App() {
+    return (
+        <div className="mainApp">
+            <HeaderContainer/>
+            <FooterContainer />
+        </div>
+    );
+}
+
+export default App;
---
+++ blog-fron-end/src/containers/FooterContainer/FooterContainer.js
@@ -0,0 +1,11 @@
+import { Container } from 'react-bootstrap';
+
+const FooterContainer = () => (
+    <footer>
+        <div className='fixed-bottom text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+            Copyright © 2022 Dominykas
+        </div>
+    </footer>
+);
+
+export default FooterContainer;
