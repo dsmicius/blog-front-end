a63bd5135e1e2994d20bf34787cf9c6b4b5a95ba
Dominykas <dsmicius@gmail.com>
2022-07-06 16:36:10 +0300

    created newBlog form. OnSubmit show entered info in CreateBlog page and console

---
+++ blog-fron-end/src/components/FormLabelControl/FormLabelControl.js
@@ -0,0 +1,27 @@
+import { Form } from 'react-bootstrap';
+
+const FormLabelControl = ({
+                              className,
+                              name,
+                              labelText,
+                              placeholderText,
+                              onChange,
+                              isTextArea = false,
+                          }) => {
+    return (
+        <Form.Group className={className} controlId={name}>
+            <Form.Label>{labelText}</Form.Label>
+            {isTextArea
+                ? <Form.Control placeholder={placeholderText}
+                                name={name}
+                                as="textarea"
+                                onChange={onChange} />
+                : <Form.Control placeholder={placeholderText}
+                                name={name}
+                                onChange={onChange} />
+            }
+        </Form.Group>
+    )
+}
+
+export default FormLabelControl;
--- blog-fron-end/src/containers/Pages/NewBlogPage/NewBlogPage.js
+++ blog-fron-end/src/containers/Pages/NewBlogPage/NewBlogPage.js
@@ -1,6 +1,70 @@
+import {Button, Container, Form} from "react-bootstrap";
+import FormLabelControl from "../../../components/FormLabelControl/FormLabelControl";
+import {useState} from "react";
+
+
 const NewBlogPage = () => {
+    const [blog, setBlog] = useState({
+        subject: '',
+        description: ''
+    });
+
+    const [visible, setVisible] = useState(false);
+
+    const handleChange = (e) => {
+        setBlog({
+            ...blog,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(blog);
+
+        setVisible(true);
+    };
+
+    const showCreatedBlogInfo = () => {
+        const {subject, description} = blog;
+        return visible &&
+            <>
+                <hr/>
+                <div>
+                    Sukurtas Blog irasas:
+                    <div><b>Subject:</b> {subject}</div>
+                    <div><b>Description:</b> {description}</div>
+                    <hr/>
+                </div>
+            </>;
+    }
+
     return (
-        <h1>Sukurti nauja Blog irasa</h1>
+        <Container>
+            <Form onSubmit={onSubmit}>
+                <FormLabelControl
+                    className="mb-3"
+                    onChange={handleChange}
+                    placeholderText="Write subject"
+                    name="subject"
+                    labelText="Blog subject"
+                />
+
+                <FormLabelControl
+                    className="mb-3"
+                    onChange={handleChange}
+                    placeholderText="Write description"
+                    name="description"
+                    labelText="Blog description"
+                    isTextArea
+                />
+                <Button variant='primary' type='submit'>
+                    Submit
+                </Button>
+            </Form>
+            {showCreatedBlogInfo()}
+        </Container>
     )
 }
 
